f3ff9d6848efba2647ad96406513d6fc3bf5aa55
durgesh <durgeshyaduvanshi108@gmail.com>
2022-05-05 11:25:10 +0530

    First  project demo...

---
+++ src/demoProject/Example1.java
@@ -0,0 +1,12 @@
+package demoProject;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int sum= a+b;
+		
+		System.out.println("Print the number"+sum);
+	}
+}
